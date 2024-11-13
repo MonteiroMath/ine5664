c2d8487f745bcac71a29ff960172d4418dd69ba6
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-11-13 13:24:54 -0300

    Implementação básica - neurônio de mcculloch

--- rna.py
+++ rna.py
@@ -12,3 +12,24 @@ Steps:
 
 - Try implementing a simple network with a sigmoid function and see where we get
 '''
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1 + np.exp(np.dot(-1, x)))
+
+VIES = 3
+W0 = 1
+
+inputArray = np.array([W0])
+weightArray = np.array([VIES])
+
+neuron_output = np.dot(inputArray, weightArray)
+
+activation_output = sigmoid(neuron_output)
+
+print(activation_output)
+
+
+
+
+
