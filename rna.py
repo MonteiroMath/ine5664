0f48775488fff9884d12da73f8ec3cf0bf887d5c
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-11-13 13:14:40 -0300

    Anotações iniciais

---
+++ rna.py
@@ -0,0 +1,14 @@
+'''
+Steps:
+
+- Define representation of neurons
+- Define representation of layers
+- Define how to connect neurons
+- Define number of neurons and layers
+- Define activation functions
+- Define loss functions
+- Define backward propagation method
+- Define training function and datasets for use
+
+- Try implementing a simple network with a sigmoid function and see where we get
+'''
