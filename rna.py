e1cd070bc2b8531b2f6aa6c28c958c9860350f61
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-11-25 12:56:30 -0300

    Comentários e pequenos ajustes para RNA

--- rna.py
+++ rna.py
@@ -3,37 +3,79 @@ from activation import activationFunctions
 
 
 def prepareInput(observation):
-    # Inclui X0 = 1 no array de observações para multiplicar pelo BIAS.
+    """
+    Inclui X0 = 1 no array de observações para multiplicar pelo BIAS.
+
+    observation consiste em um array contendo os atributos de uma observação
+    """
     return np.concatenate(([1], observation))
 
 
 def forwardPass(input, weights, activationFunction):
+    """
+    Realizar um forward pass para uma camada
 
-    adjustedInput = prepareInput(input)
+    Recebe:
 
-    combination = np.dot(adjustedInput, weights.T)
+        input: Array contendo as entradas da camada (atributos do input ou ativações da camada anterior)
+
+        weights: matrix contendo os pesos de cada neurônio da camada
+
+        activationFunction: função de ativação definida para uso na camada
+
+    Retorna:
+
+        activation: os valores calculados para a ativação do neurônio
+
+        combination: os valores calculados na combinação linear do neurônio
+    """
+
+    combination = np.dot(input, weights.T)
     activation = activationFunction(combination)
 
-    return activation, combination, adjustedInput
+    return activation, combination
 
 
 def rna(input, layers):
+    """
+    Implementa a etapa de forward propagation da rede neural
 
-    intermediateValues = []
+    Recebe:
 
+        input: Array contendo os atributos da observação para processamento
+
+        layers: tupla contendo "weights" e "functions". Ver initLayers.
+
+    """
+
+    # Array para guardar valores intermediários para retropropagação
+    intermediateValues = [] 
+
+    # inicializa prevActivations com os valores da camada de input
     prevActivations = input
 
+    # extrai listas de pesos e funções
     weights, functions = layers
 
+    # iteração pelas camadas da rede neural
     for i in range(len(weights)):
 
+        # Extrai os pesos da camada
         layerWeights = weights[i]
+
+        # Extrai a função de ativação da camada
         activationFunction, derivativeFunction = functions[i]
 
-        activations, combinations, layerInput = forwardPass(
-            prevActivations, layerWeights, activationFunction)
+        layerInput = prepareInput(prevActivations)
 
+        # Realiza a forwardPass da camada
+        activations, combinations = forwardPass(
+            layerInput, layerWeights, activationFunction)
+
+        # Guarda os valores intermediários produzidos
         intermediateValues.append((layerInput, combinations))
+
+        # atualiza prevActivations para os valores de ativação produzidos nessa camada
         prevActivations = activations
 
     return activations, intermediateValues
