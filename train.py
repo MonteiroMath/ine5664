d8096014f652f0dc61798b5ada0b8df7129b4cfd
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-11-25 11:08:01 -0300

    Housekeeping - remoção de código morto

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import numpy as np
 from rna import rna
 from cost import costFunctions
-from activation import activationFunctions
 from initLayers import initLayers
 from backpropagation import backpropagation
 
@@ -9,18 +8,14 @@ from backpropagation import backpropagation
 def train(epochs, learningRate, layers, observations):
 
     costFunction, costDerivative = costFunctions["MSE"]
-    activationFunction, activationDerivative = activationFunctions["SIGMOID"]
 
     for n in range(epochs):
-
-        # print(weights['output_layer_weights'])
 
         predictions = []
         for observation in observations:
 
             attributes, label = observation
             attributes = np.array(attributes)
-            # prediction, combination, ol_input = rna(attributes, weights)
             prediction, intermediateValues = rna(attributes, layers)
             predictions.append(prediction)
 
@@ -44,28 +39,9 @@ layers = [
 
 layers = initLayers(layers, 2)
 
-
-'''
-np.random.seed(42)
-layer_1_weights = np.random.randn(2, 3)
-np.random.seed(500)
-layer_2_weights = np.random.randn(2, 3)
-np.random.seed(22)
-output_layer_weights = np.random.randn(1, 3)
-'''
-
-
 EPOCHS = 1000
 LEARNING_RATE = 0.1
 
-'''
-START_WEIGHTS = {
-    'layer_1_weights': layers[0][0],
-    'layer_2_weights': layers[1][0],
-    'output_layer_weights': layers[2][0],
-
-}
-'''
 observations = [
     ([0, 0], 0),
     ([0, 1], 1),
