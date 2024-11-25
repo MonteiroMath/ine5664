bad41899545d0924c4af5384557f9434fd5c4fd2
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-11-25 12:27:39 -0300

    Ajuste do arquivo main.py

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-from rna import train
+from train import train
+from initLayers import initLayers
 
-'''
-START_WEIGHTS = [0, 0, 0]
-LEARNING_RATE = 0.5
-
-observations = [
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1],
+# Lista no formato [(neurons, activation), (neurons, activation), (neurons, activation)]
+# Cada tupla representa uma camada
+layers = [
+    (2, 'SIGMOID'),
+    # (2, 'SIGMOID'),
+    (1, 'SIGMOID')
 ]
 
-labels = [0, 1, 1, 1]
+costF = "MSE"
 
-final_weights = train(10, LEARNING_RATE, START_WEIGHTS, observations, labels)
+layers = initLayers(layers, 2)
 
-print(final_weights)
-'''
+EPOCHS = 1000
+LEARNING_RATE = 0.1
+
+observations = [
+    ([0, 0], 0),
+    ([0, 1], 1),
+    ([1, 0], 1),
+    ([1, 1], 1),
+]
+
+
+train(EPOCHS, LEARNING_RATE, layers, observations, costF)
--- train.py
+++ train.py
@@ -29,29 +29,3 @@ def train(epochs, learningRate, layers, observations, costF):
             adjustedWeights = backpropagation(
                 layers, intermediateValues, costD, learningRate)
             layers[0] = adjustedWeights
-
-
-# Lista no formato [(neurons, activation), (neurons, activation), (neurons, activation)]
-# Cada tupla representa uma camada
-layers = [
-    (2, 'SIGMOID'),
-    # (2, 'SIGMOID'),
-    (1, 'SIGMOID')
-]
-
-costF = "MSE"
-
-layers = initLayers(layers, 2)
-
-EPOCHS = 1000
-LEARNING_RATE = 0.1
-
-observations = [
-    ([0, 0], 0),
-    ([0, 1], 1),
-    ([1, 0], 1),
-    ([1, 1], 1),
-]
-
-
-train(EPOCHS, LEARNING_RATE, layers, observations, costF )
