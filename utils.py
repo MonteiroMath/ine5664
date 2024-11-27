f56703bdd3b911229b136922d424e77aeffd8b50
electrolisis <79479589+electrolisis@users.noreply.github.com>
2024-11-27 16:49:43 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,20 @@
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 def accuracy(predictions, labels):
     return int(sum(labels == predictions) / len(labels) * 100)
 
-
 def normalize(data, feature_range=(0, 1)):
 
-    data_min = np.min(data, axis=0)
-    data_max = np.max(data, axis=0)
-    scale = feature_range[1] - feature_range[0]
-    normalized_data = (data - data_min) / (data_max -
-                                           data_min) * scale + feature_range[0]
-    return normalized_data, {"min": data_min, "max": data_max, "range": scale}
+    data = np.array(data)
+    scaler = MinMaxScaler(feature_range=feature_range)
+    normalized_data = scaler.fit_transform(data)
+    return normalized_data, scaler
 
 
-def denormalize(normalized_data, params, feature_range=(0, 1)):
-    data_min = params["min"]
-    data_max = params["max"]
-    scale = params["range"]
-    denormalized_data = (
-        normalized_data - feature_range[0]) / scale * (data_max - data_min) + data_min
+def denormalize(normalized_data, scaler):
+
+    normalized_data = np.array(normalized_data)
+    denormalized_data = scaler.inverse_transform(normalized_data)
+
     return denormalized_data
