6edfa36d2ba891bc4e6a0782f64f7d29240c82e9
electrolisis <79479589+electrolisis@users.noreply.github.com>
2024-11-27 16:52:38 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score
 
 def accuracy(predictions, labels):
     return int(sum(labels == predictions) / len(labels) * 100)
@@ -18,3 +19,17 @@ def denormalize(normalized_data, scaler):
     denormalized_data = scaler.inverse_transform(normalized_data)
 
     return denormalized_data
+
+def evaluate_regression(targets, predictions):
+
+    targets = np.array(targets)
+    predictions = np.array(predictions)
+
+    # Compute metrics
+    mse = mean_squared_error(targets, predictions)  # Mean Squared Error
+    rmse = np.sqrt(mse)                    # Root Mean Squared Error
+    r2 = r2_score(targets, predictions)  # R^2 Score
+
+    # Return metrics in a dictionary
+    return {"RMSE": rmse, "R^2": r2}
+    
