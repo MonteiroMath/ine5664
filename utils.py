9a009a7ff125abb5013f4a3c70609a992fb5ea41
BrunoDuarteDB <brunodborja4@gmail.com>
2024-11-29 15:46:56 -0300

    Add evaluate_classification at utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
 
 def accuracy(predictions, labels):
     return int(sum(labels == predictions) / len(labels) * 100)
@@ -41,4 +43,20 @@ def splitData(features, labels, validationPercentage=0.2):
         test_size=validationPercentage,
     )
     return featuresTrain, featuresVal, targetTrain, targetVal
+
+
+
+def evaluate_classification(labels, predictions):
+    labels = np.array(labels)
+    predictions = np.array(predictions)
+
+    metrics = {
+        "Accuracy": accuracy_score(labels, predictions),
+        "Precision": precision_score(labels, predictions),
+        "Recall": recall_score(labels, predictions),
+        "F1 Score": f1_score(labels, predictions),
+
+    }
+    return metrics
+
     
