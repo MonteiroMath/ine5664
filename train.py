611f348f8e3d6da1fe2bcca939853c527dc87159
electrolisis <79479589+electrolisis@users.noreply.github.com>
2024-12-04 14:56:55 -0300

    Update train.py

--- train.py
+++ train.py
@@ -32,6 +32,8 @@ def train(epochs, learningRate, layers, observations, labels, costF):
             CATEGORICAL_ENTROPY
     """
 
+    error = []
+    
     # extrai a função de custo e sua derivada
     costFunction, costDerivative = costFunctions[costF]
 
@@ -67,5 +69,19 @@ def train(epochs, learningRate, layers, observations, labels, costF):
         predictions = np.array(predictions)
         cost = costFunction(predictions, labels)
         print(cost)
+        error.append(cost)
+
+        #
+        from matplotlib import pyplot as plt
+        plt.plot(error)
+        if (costF == "MSE"):
+            plt.title("regressão - treino")
+        elif (costF == "BINARY_ENTROPY"):
+            plt.title("classificação binária - treino")
+        else:
+            plt.title("classificação multiclasse - treino")
+        plt.xlabel("epoca")
+        plt.ylabel("custo")
+        plt.show()
 
     return layers
